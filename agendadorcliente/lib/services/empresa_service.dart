8f04d884e272f5f6e4d29a9abc322ff63833389f
Fialkoski <jean.fialkoski@gmail.com>
2025-10-28 10:46:53 -0300

    removido classes

--- agendadorcliente/lib/services/empresa_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:agendadorcliente/models/empresa.dart';
-import 'package:agendadorcliente/services/api_service.dart';
-
-class EmpresaService {
-  Future<Empresa> buscarEmpresaPorCpfcnpj(String cpfcnpj) async {
-    return ApiService.buscar<Empresa>(
-        '/api/empresas/cpfcnpj/$cpfcnpj', Empresa.fromJson);
-  }
-}
